3f4eaeb39d2e6f5969d1ff46c2ff9440076f51d9
bkalender <bthnklndr@gmail.com>
2018-10-06 20:34:34 +0300

    Cube & Spell added.

---
+++ Assets/Scripts/Spell.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour {
+
+    [SerializeField] Cube cube;
+    [SerializeField] float launchX = 2f;
+    [SerializeField] float launchY = 20f;
+
+    Vector3 cubeToBallVector;
+    bool hasStarted = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        cubeToBallVector = transform.position - cube.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockSpellToPlayer();
+            LaunchSpellOnMouseClick();
+        }
+    }
+
+    private void LockSpellToPlayer()
+    {
+        Vector3 cubePos = new Vector3(cube.transform.position.x, cube.transform.position.y, cube.transform.position.z);
+        transform.position = cubePos + cubeToBallVector;
+    }
+
+    private void LaunchSpellOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(launchX, launchY);
+        }
+    }
+}
